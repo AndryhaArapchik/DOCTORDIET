a54dc00d3b3451ced50c46ac956241107d84dcbf
andryhaarapchik <andryhaarapchik@gmail.com>
2021-02-18 05:58:23 +0200

    Пятый экран

--- dist/main.d8ebb8d6.js
+++ dist/main.d8ebb8d6.js
@@ -12034,7 +12034,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57552" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57052" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
