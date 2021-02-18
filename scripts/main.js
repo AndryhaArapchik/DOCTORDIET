e1c1c2696950fa8f9eed295e1a933b679bcdbf9a
andryhaarapchik <andryhaarapchik@gmail.com>
2021-02-18 05:14:52 +0200

    Слайдер с адаптацией

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
+ import Swiper from 'swiper/bundle';
+ import 'swiper/swiper-bundle.css';
+
 document.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad(){
-    
+    const swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 }
